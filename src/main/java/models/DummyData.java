c4af84099d5b5adaae478ce6a66d3a497b8efff7
Swolebrain <thecodingteacher@gmail.com>
2018-02-24 20:47:49 -0500

    fix: off by 1 bug

--- src/main/java/models/DummyData.java
+++ src/main/java/models/DummyData.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class DummyData {
     public static List<ArrayList<Event>> eventPages = new ArrayList<>();
     static {
+        eventPages.add(new ArrayList<>());
         for (int i = 1; i < 100; i++){
             ArrayList<Event> newPage = new ArrayList<>();
-            for (int j = 0; j < (i == 1 ? 20 : 4); j++){
+            int maxJ = (i == 1 ? 20 : 4);
+            for (int j = 0; j < maxJ; j++){
                 newPage.add(new Event(i));
             }
             eventPages.add(newPage);
